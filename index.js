39f670a11563174865fce7af024106eac8574ced
lynnjaco <jacobs.lynnette@gmail.com>
2024-01-07 17:55:19 -0500

    Accumulator Pattern Lab

--- index.js
+++ index.js
@@ -3,7 +3,16 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+
+  let acc = true;
+  for (let roll of rolls) {
+    if(isNaN(roll)) {
+      acc = false;
+    }
+  }
+return acc;
+}
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,7 +20,16 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+
+function findValue(rolls, value) {
+  let acc = null;
+  for (let roll of rolls) {
+    if (roll === value) {
+      return acc = value;
+    }
+  }
+  return acc;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,7 +37,15 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let acc = [];
+  for (let roll of rolls) {
+    if (roll >= lowest) {
+      acc.push(roll);
+    }
+  }
+  return acc;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
@@ -43,8 +69,7 @@ function getRollCounts(rolls) {
       console.log("after:", rollCount);
     }
   }
-
-  return;
+  return rollCount;
 }
 
 // Do not change the code below here.
