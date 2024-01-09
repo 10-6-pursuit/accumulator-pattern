ff7e5f547447cb1ff8aec9a13846b50e882932b1
MikesKids <michael.dantzler@gmail.com>
2024-01-09 17:42:00 -0500

    Completed third function

--- index.js
+++ index.js
@@ -36,7 +36,15 @@ function findValue(rolls, value) {
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  let higherThanLowArray = [];
+  for (let roll of rolls) {
+    if (roll >= lowest) {
+      higherThanLowArray.push(roll);
+    }
+  }
+  return higherThanLowArray;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
