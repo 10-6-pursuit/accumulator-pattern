30ab649bb7eafc005447b430f2d7a159404c43b6
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-01-10 22:03:36 -0500

    Function 2 complete

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function isValid(rolls) {
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  if (!rolls.length) return null;
+
+  return rolls.find(num => num === value) || null;
+}
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
