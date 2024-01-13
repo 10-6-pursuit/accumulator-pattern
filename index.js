92f98d8c53d64ddccf37dd1e6bf6ef6710b5c1b6
kurtCoding <kjulien7790@gmail.com>
2024-01-12 22:55:20 -0500

    Accumulator-lab-Completed

--- index.js
+++ index.js
@@ -3,15 +3,27 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
-
+function isValid(rolls) {
+    const isNumber = (roll) => typeof roll === "number";
+    const allNumbers = rolls.every(isNumber)
+  return allNumbers;
+}
+// console.log(isValid());
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
+function findValue(rolls, value) {
+  for (let i = 0; i < rolls.length; i++) {
+    if (rolls[i] === value) {
+     return rolls[i];
+   }
+ }
+   return null;
+}
+  
 
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
@@ -19,7 +31,15 @@ function findValue(rolls, value) {}
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  const higherVal = [];
+  for (let roll of rolls) {
+    if (roll >= lowest) {
+      higherVal.push(roll);
+    }
+  }
+  return higherVal;
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
@@ -30,23 +50,12 @@ function getRollCounts(rolls) {
   const rollCount = {};
 
   for (let roll of rolls) {
-    // first time seeing number
     if (!rollCount[roll]) {
-      console.log("before:", rollCount);
       rollCount[roll] = 1;
-      console.log("after:", rollCount);
-
-      // number has been seen already
-    } else {
-      console.log("before:", rollCount);
-      rollCount[roll] += 1;
-      console.log("after:", rollCount);
-    }
+    } else rollCount[roll]++;
   }
-
-  return;
+  return rollCount;
 }
-
 // Do not change the code below here.
 module.exports = {
   isValid,
